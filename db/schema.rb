be7d8aab1f4d007988d9891347be00caf0d88dc2
siddujajee <jajeesiddu2002@gmail.com>
2024-01-27 13:50:10 +0530

    pg

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_01_25_081628) do
-  create_table "dummy_posts", force: :cascade do |t|
-    t.string "title"
-    t.string "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
-  create_table "posts", force: :cascade do |t|
+  create_table "dummy_posts", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
