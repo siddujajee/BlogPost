836307198171cd52dd01b1090162171eae941fc0
siddujajee <jajeesiddu2002@gmail.com>
2024-02-06 21:25:57 +0530

    nested comment

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,6 +1,6 @@
 class Feedback < ApplicationRecord
   belongs_to :dummy_post
   belongs_to :user
-  belongs_to :parent, class_name:"Feedback", allow_nil: true
+  belongs_to :parent, class_name:"Feedback", optional: true
   has_many :feedbacks, foreign_key: :parent_id
 end
