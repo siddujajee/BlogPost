01f5a64cd9ba68d3a91c12ee48c2090d51769569
siddujajee <jajeesiddu2002@gmail.com>
2024-02-08 16:45:46 +0530

    ,

--- db/migrate/20240208110606_update_user_id.rb
+++ db/migrate/20240208110606_update_user_id.rb
@@ -1,8 +1,10 @@
 class UpdateUserId < ActiveRecord::Migration[7.1]
 
     def up
+      ActiveRecord::Base.transaction do
       Feedback.where(user_id: nil).update_all(user_id: 1)
      change_column :feedbacks, :user_id, :integer, null: false, default: 1
+      end
      end
       def down
      change_column :feedbacks, :user_id, :integer, null: true, default: nil
