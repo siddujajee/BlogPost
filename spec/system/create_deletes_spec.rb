45b1a743d5f1948c6943bd027a2fb847503b2c0a
siddujajee <jajeesiddu2002@gmail.com>
2024-02-13 00:56:13 +0530

    create and delete post capybara testing

--- spec/system/create_deletes_spec.rb
+++ spec/system/create_deletes_spec.rb
@@ -1,22 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "CreateDeletes", type: :system do
-  it "creates post" do
-user=create(:user, email:"example@gmail.com", password:"password")
-login_as(user)
-visit "/"
-click_on "New Blog Post"
-visit "/dummy_posts/new"
-fill_in "Title", with:"this is capybara testing post title"
-# expect(page).to have_field("Content", wait: 10)
-find("#dummy_post_content").set("this is capybara testing post's content")
-click_on "Create Dummy post"
-
-expect(page).to have_current_path(dummy_post_path(DummyPost.last))
-
-  end
-
-it "deletes post" do
+it "creates and deletes post" do
   user=create(:user, email:"example@gmail.com", password:"password")
 login_as(user)
 visit "/"
@@ -26,6 +11,7 @@ fill_in "Title", with:"this is capybara testing post title"
 # expect(page).to have_field("Content", wait: 10)
 find("#dummy_post_content").set("this is capybara testing post's content")
 click_on "Create Dummy post"
+post=create(:dummy_post, title:"this is capybara testing post title", content:"this is capybara testing post's content")
 click_on "edit"
 visit edit_dummy_post_path(DummyPost.last)
 click_on "delete"
